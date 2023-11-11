e3bb3199917d4d1262e87fa7428689ef476d483b
Sayan Halder <>
2023-11-11 11:26:44 +0530

    change massage publish interval

--- config/mqtt.js
+++ config/mqtt.js
@@ -74,7 +74,7 @@ client.on("connect", async (connack) => {
         error
       );
     });
-  setInterval(publishMessage, 10000); //publishing the fake massage to the broker
+  setInterval(publishMessage, 100000); //publishing the fake massage to the broker
 });
 
 client.on("message", async (topic, payload) => {
